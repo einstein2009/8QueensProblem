21c9df9be2c7aea929c593440d4b59bc71610b1c
zephyr9990 <34167795+zephyr9990@users.noreply.github.com>
2017-12-09 11:53:44 -0800

    Update main.cpp

--- 8QueensProblem/main.cpp
+++ 8QueensProblem/main.cpp
@@ -5,14 +5,13 @@
 *                 Nicholas Ackors, Stephen Hall            *
 *  Course:        CS322 Data Structure and Algorithms II   *
 *  Assignment:    Module 7, Written Assignment             *
-*  Program:       8 Queens problem				       	   *
+*  Program:       8 Queens problem		           *
 *  Description:   Creates a chessboard populated with 8	   *
-*				  queens in such a manor that no queen can *
-*				  capture another.						   *
+*		  queens in such a manor that no queen can *
+*		  attack another.                          *
 *  Input:         Takes no input, simply displays options. *
-*  Output:        Chess Board that has the locations of 8  *
-*				  queens marked. There can be no way for   *
-*				  any queen to take another.			   *
+*  Output:        Displays a simulated chessboard with the *
+*                 location of all eight queens.            *
 *  Created:       12/9/2017                                *
 *                                                          *
 ***********************************************************/
@@ -23,8 +22,16 @@ using namespace std;
 
 int main()
 {
-
-
-	system("PAUSE");
+	ChessBoard<string> *CpChessBoardPtr = new ChessBoard<string>;
+	
+	CpChessBoardPtr->placeEightQueens();
+	CpChessBoardPtr->display();
+	
+	//clears buffer and terminates program on next input
+	cout << "Terminating program... press any key to exit.";
+	cin.clear();
+	cin.ignore(100, '\n');
+	cin.get();
+	
 	return 0;
 }
